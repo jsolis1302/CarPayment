debf03228e5a70679510d515ddc8daf473b72d04
Jorge Solis <solisjorge1302@gmail.com>
2016-06-02 16:00:20 -0500

    Add files via upload
    index and log in files

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Template</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<h1>Index</h1>
+	<nav class ="navbar navbar-inverse" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle " data-toggle="collapse" 
+				data-target="#example-navbar-collapse">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+          		</button>
+          		<a href="index.php" class="navbar-brand">Index</a>	
+			</div>
+			<div class="collapse navbar-collapse" id="example-navbar-collapse">
+				<ul class="nav navbar-nav">
+		            <li><a href="../views/NewCollection.php">Pagar</a></li>
+		            <li><a href="../views/NewObject.php">Consultar</a></li>
+          		</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<p>Aqui va el contenido</p>
+	</div>
+	
+</body>
+</html>
