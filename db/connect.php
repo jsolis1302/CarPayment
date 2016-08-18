f90a329d2ecf32c875275bc08b01571c126e1205
Jorge Solis <solisjorge1302@gmail.com>
2016-08-18 17:15:18 -0500

    Add files via upload
    connect function

---
+++ db/connect.php
@@ -0,0 +1,19 @@
+<?php
+
+   function connect() {
+      $dbhost = 'localhost';
+      $dbuser = 'root';
+      $dbpass = '';
+      $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+      
+      if(! $conn ) { 
+         die('Could not connect: ' . mysqli_error());
+      }
+      
+      echo 'Connected successfully';
+      
+      mysqli_select_db( $conn,'carpay' );
+      mysqli_close($conn);
+}
+   
+?>
