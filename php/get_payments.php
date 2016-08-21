1a9620ae3323a17bd316ca5c7b19532acc36edcc
Jorge Solis <solisjorge1302@gmail.com>
2016-08-20 19:39:56 -0500

    Modified query to get payments

--- php/get_payments.php
+++ php/get_payments.php
@@ -5,7 +5,7 @@
    
    $link = connect();
    //$link  = mysqli_connect("localhost","root","root","carpay");
-   $sql = 'SELECT pay_date, amount, user_name FROM view_payment_users';
+   $sql = "SELECT date_format(pay_date, '%d %b %Y') as pay_date, amount, user_name FROM view_payment_users";
    $retval = mysqli_query( $link,$sql);
 
    if(! $retval ) {
