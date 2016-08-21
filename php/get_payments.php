163331783d3058e7cbf8e6bfecf628cccfea7784
Jorge Solis <solisjorge1302@gmail.com>
2016-08-21 18:46:01 -0500

    added some comments

--- php/get_payments.php
+++ php/get_payments.php
@@ -6,6 +6,10 @@
    $link = connect();
    //$link  = mysqli_connect("localhost","root","root","carpay");
    $sql = "SELECT date_format(pay_date, '%d %b %Y') as pay_date, amount, user_name FROM view_payment_users";
+
+   
+   //$sql_2 = "select sum(amount) as total from tbl_payment;"
+   //$retval2 = mysqli_query($link,$sql2);
    $retval = mysqli_query( $link,$sql);
 
    if(! $retval ) {
