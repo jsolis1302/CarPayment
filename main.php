494a100bf18f6841ca64e3de890fdde67575419c
Jorge Solis <solisjorge1302@gmail.com>
2016-12-13 14:13:03 -0600

    main
    added logout validation

--- main.php
+++ main.php
@@ -51,7 +51,16 @@
             <li><a href="#">Dashboard</a></li>
             <li><a href="#">Settings</a></li>
             <li><a href="#">Profile</a></li>
-            <li><a href="php/logout.php">Log Out</a></li>
+            <li> <?php 
+                   if (! empty($_SESSION["login"])) 
+                      {
+                        echo '<a href="php/logout.php" title="Cerrar sesión">'.$_SESSION["login"].'</a>'; 
+
+                      }
+
+            ?> 
+            <!--<a href="">Log Out</a>-->
+            </li>
           </ul>
           <form class="navbar-form navbar-right">
             <input type="text" class="form-control" placeholder="Search...">
